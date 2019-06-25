81a060afc876207abe7d69458a7a62516ebfca8b
ivan-kharuta <42738591+ivan-kharuta@users.noreply.github.com>
2019-06-26 00:45:47 +0300

    Homework 1 - components

---
+++ vue-js-hw1/src/components/MainContainer.vue
@@ -0,0 +1,106 @@
+<template>
+  <main class="main-container">
+    <div class="filter-bar"></div>
+
+    <div class="movies-list">
+      <div class="movie-item">
+        <div class="movie-item__poster"></div>
+        <div class="movie-item__year">2004</div>
+        <div class="movie-item__title">Four rooms</div>
+        <div class="movie-item__genre">Action & Adventure</div>
+      </div>
+
+      <div class="movie-item">
+        <div class="movie-item__poster"></div>
+        <div class="movie-item__year">2003</div>
+        <div class="movie-item__title">Jackie Brown</div>
+        <div class="movie-item__genre">Action & Adventure</div>
+      </div>
+
+      <div class="movie-item">
+        <div class="movie-item__poster"></div>
+        <div class="movie-item__year">1994</div>
+        <div class="movie-item__title">Bill:Vol 2</div>
+        <div class="movie-item__genre">Oscar winning Movie</div>
+      </div>
+
+      <div class="movie-item">
+        <div class="movie-item__poster"></div>
+        <div class="movie-item__year">2004</div>
+        <div class="movie-item__title">Pulp Fiction</div>
+        <div class="movie-item__genre">Action & Adventure</div>
+      </div>
+
+      <div class="movie-item">
+        <div class="movie-item__poster"></div>
+        <div class="movie-item__year">2003</div>
+        <div class="movie-item__title">Bill:Vol 1</div>
+        <div class="movie-item__genre">Action & Adventure</div>
+      </div>
+
+      <div class="movie-item">
+        <div class="movie-item__poster"></div>
+        <div class="movie-item__year">1994</div>
+        <div class="movie-item__title">Reservoir dogs</div>
+        <div class="movie-item__genre">Oscar winning Movie</div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainContainer"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.main-container {
+  background-color: #212121;
+}
+.filter-bar {
+  height: 100px;
+  background-color: #424242;
+}
+
+.movies-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 40px;
+}
+
+.movie-item {
+  width: 30%;
+  margin-bottom: 40px;
+  color: #9E9E9E;
+  text-align: left;
+}
+
+.movie-item__poster {
+  padding-top: 130%;
+  margin-bottom: 20px;
+  background-color: #455A64;
+
+  &:after {
+    content: '';
+    clear: both;
+  }
+}
+
+.movie-item__year {
+  display: inline-block;
+  float: right;
+  padding: 3px 10px;
+  border: 1px solid #9E9E9E;
+  border-radius: 5px;
+}
+
+.movie-item__title {
+  margin-bottom: 15px;
+}
+</style>
---
+++ vue-js-hw1/src/components/MainHeader.vue
@@ -0,0 +1,66 @@
+<template>
+  <header class="main-header">
+    <div class="search-bar">
+      <input
+        type="text"
+        class="search-bar__input"
+        @keyup.enter="findTheMovie"
+      />
+      <button class="search-bar__btn" @click.prevent="findTheMovie">Search</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+  methods: {
+    findTheMovie() {
+      alert('Success!');
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.main-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 400px;
+  background-color: #000;
+  background-image: url("https://images.amediateka.ru/image_d12378e2-8678-4b9d-8f32-07f7d74b81b8.jpg");
+  background-size: cover;
+  background-position: center;
+}
+
+.search-bar {
+  display: flex;
+  width: 80%;
+  max-width: 900px;
+}
+
+.search-bar__input {
+  flex-grow: 1;
+  height: 50px;
+  margin-right: 10px;
+  padding: 0 15px;
+  color: #fff;
+  font-size: 18px;
+  border: none;
+  border-radius: 5px;
+  background-color: rgba(66,66,66,0.8);
+}
+
+.search-bar__btn {
+  height: 50px;
+  padding: 10px 50px;
+  text-transform: uppercase;
+  font-size: 20px;
+  color: #fff;
+  background-color: #D81B60;
+  border: none;
+  border-radius: 5px;
+}
+</style>
